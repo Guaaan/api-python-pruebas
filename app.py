812a4065cfcf6c7360b05dd3fc995916a54933e3
guaaan <juampablo.txt@gmail.com>
2021-09-28 10:50:18 -0300

    enviar cambios

--- app.py
+++ app.py
@@ -21,9 +21,21 @@ def getProduct(product_name):
 
 @app.route('/products', methods=['POST'])
 def addProduct():
-    print(request.json)
-    return 'recieved'
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({"message":"Product Added Succesfully", "products": products})
 
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+    if(len(productFound) > 0):
+        productFound[0]['name'] = request.json['name']
+        productFound[0]['price'] = request.json['price']
+        productFound[0]['quantity'] = request.json['quantity']
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
