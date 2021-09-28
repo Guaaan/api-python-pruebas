50a0524280378590f743847114cc890f63ad8d33
guaaan <juampablo.txt@gmail.com>
2021-09-28 10:29:23 -0300

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,20 @@ def ping():
 
 @app.route('/products')
 def getProducts():
-    return jsonify
+    return jsonify({"products": products, "message": "products list"})
+
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [product for product in products if product['name'] == product_name]
+    if (len(productsFound) > 0):
+        return jsonify({"product": productsFound[0]}) 
+    return jsonify({"message": "Product not found"})
+
+@app.route('/products', methods=['POST'])
+def addProduct():
+    print(request.json)
+    return 'recieved'
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
